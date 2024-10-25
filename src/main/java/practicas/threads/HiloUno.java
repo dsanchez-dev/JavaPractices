24903e84be319db60cfc884711848fa6adf3b1d2
Daniel Sanchez Gonzalez <sanchezgonzalezdaniel3@gmail.com>
2024-10-25 13:22:26 -0600

    Uso de Thread Interrupted

--- src/main/java/practicas/threads/HiloUno.java
+++ src/main/java/practicas/threads/HiloUno.java
@@ -4,7 +4,17 @@ public class HiloUno extends Thread {
 
     @Override
     public void run(){
-        System.out.println("hola desde el hilo uno");
+        for(;;){
+            int i = 0;
+            boolean interrumpido = isInterrupted();
+            if(interrumpido){
+                System.out.println("El hilo fue interrumpido");
+                return;
+            }
+            if (++i == 10){
+                System.out.println("Hilo uno siendo interrumpido");
+            }
+        }
     }
 
 }
--- src/main/java/practicas/threads/UsoDeLosHilos.java
+++ src/main/java/practicas/threads/UsoDeLosHilos.java
@@ -1,24 +1,13 @@
 package practicas.threads;
 
 public class UsoDeLosHilos {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        System.out.println("Hilos de forma larga");
         HiloUno nuevoHilo = new HiloUno();
-        HiloDos nuevoHilo2 = new HiloDos();
-
         nuevoHilo.start();
-        System.out.println("-------Hola-------");
-        nuevoHilo2.run();
 
+        nuevoHilo.interrupt();
 
-
-        Thread hilo1 = new Thread(new HiloUno());
-        Thread hilo2 = new Thread(new HiloDos());
-        System.out.println("");
-        System.out.println("Hilos de forma corta");
-        hilo1.start();
-        hilo2.start();
-
+        System.out.println("Hola");
     }
 }
